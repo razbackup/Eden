1c10d24f45ae9bbbbd46b2edc14bd1ca1eaad1a5
GodDosentPlayDice <vicevasquez95@gmail.com>
2022-11-08 22:14:06 -0300

    Subida de archivos 8/11

---
+++ src/domain/clases/Validacion.java
@@ -0,0 +1,9 @@
+package domain.clases;
+public class Validacion {
+    public boolean validName(String nombre){
+        if (nombre.length() < 1 || nombre.length() > 25){
+            return false;
+        }
+        return true;
+    }
+}
